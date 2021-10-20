42720a19bd4180dc736a5f75d0bc1e76263c3052
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-10-20 16:02:20 +0700

    Refactoring Shape area calculation

---
+++ src/csharp/CodeSmells/CodeSmells.CalculateShapeArea/ShapeAreaFactories.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodeSmells.CalculateShapeArea
+{
+    internal interface IShapeAreaFactory
+    {
+        IShapeArea Create();
+    }
+
+    internal class CircleFatory : IShapeAreaFactory
+    {
+        public IShapeArea Create()
+        {
+            Console.WriteLine("Radius: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            return new Circle(r, 2);
+        }
+    }
+
+    internal class RectangleFactory : IShapeAreaFactory
+    {
+        public IShapeArea Create()
+        {
+            Console.WriteLine("Length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            return new Rectangle(length, width);
+        }
+    }
+
+    internal class TrapezoidFactory : IShapeAreaFactory
+    {
+        public IShapeArea Create()
+        {
+            Console.WriteLine("First base: ");
+            double base1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Second base: ");
+            double base2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            return new Trapezoid(base1, base2, height);
+        }
+    }
+}
---
+++ src/csharp/CodeSmells/CodeSmells.CalculateShapeArea/ShapeAreas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CodeSmells.CalculateShapeArea
+{
+    public interface IShapeArea
+    {
+        string ShapeName { get; }
+        double GetArea();
+    }
+
+    internal class Circle : IShapeArea
+    {
+        public Circle(double radius, int roundDecimal)
+        {
+            Radius = radius;
+            RoundDecimal = roundDecimal;
+            ShapeName = nameof(Circle);
+        }
+
+        public double Radius { get; }
+        public int RoundDecimal { get; }
+        public string ShapeName { get; }
+
+        public double GetArea()
+        {
+            return Math.Round(Math.PI * Math.Pow(Radius, 2), RoundDecimal);
+        }
+    }
+
+    internal class Rectangle : IShapeArea
+    {
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+            ShapeName = nameof(Rectangle);
+        }
+
+        public double Length { get; }
+        public double Width { get; }
+
+        public string ShapeName { get; }
+
+        public double GetArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    internal class Trapezoid : IShapeArea
+    {
+        public Trapezoid(double base1, double base2, double height)
+        {
+            Base1 = base1;
+            Base2 = base2;
+            Height = height;
+            ShapeName = nameof(Trapezoid);
+        }
+
+        public double Base1 { get; }
+        public double Base2 { get; }
+        public double Height { get; }
+        public string ShapeName { get; }
+
+
+        public double GetArea()
+        {
+            return (Base1 + Base2) * Height / 2;
+        }
+    }
+}
